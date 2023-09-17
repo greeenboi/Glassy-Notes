d5f0894872641929cfa900b503489332062eb673
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-09-17 21:43:13 +0530

    Alpha 0.0.1 pre - release

--- src/App.jsx
+++ src/App.jsx
@@ -44,31 +44,11 @@ const App = () => {
     content: `
         <h1>Hi there,</h1>
         <p>
-          this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+          this is a <em>basic</em> example of <strong><I>Glassy Notes</I></strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the rest.
+          <br/>
+          do <code> CTRL + A </code> and <code> DELETE</code> or <code>BACKSPACE</code> to start typing...
         </p>
-        <ul>
-          <li>
-            That’s a bullet list with one …
-          </li>
-          <li>
-            … or two list items.
-          </li>
-        </ul>
-        <p>
-          Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-        </p>
-        <pre><code class="language-css">body {
-          display: none;
-        }</code></pre>
-        <p>
-          I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-        </p>
-        <blockquote>
-          Wow, that’s amazing. Good work, boy! 👏
-        <br />
-        <i>— Mom</i>
-        </blockquote>
-        <br/>
+        
         
             `,
   })
